480606a3eb376f9ae29c93cc228f20978b112ab7
Caflo <carlo.cannarsa@gmail.com>
2021-10-02 21:21:46 +0200

    Update link_scraper.py
    minor fixes

--- link_scraper.py
+++ link_scraper.py
@@ -1,6 +1,7 @@
 import logging, telegram
 import asyncio
 import timeit
+import sys
 import json
 import os, subprocess
 import http.client
@@ -45,23 +46,24 @@ def analyze_anchors(url, base_url, result=[], verbose=False, indent_level=0):
                 if next_url.startswith('/'): # keep navigating inside the target's page
                     next_url = base_url + next_url  # rewriting from relative to absolute path to make the curl work at the next recursion
                     if verbose:
-                        print('\t' * indent_level + next_url)
+                        print('\t' * indent_level + "|" + next_url)
                     result.append(next_url + " - INTERNAL")
                     analyze_anchors(next_url, base_url, result=result, verbose=verbose, indent_level=indent_level+1)
-                else: # TODO for all non-local URLs, collect data before continuing 
-                    next_url = base_url + next_url  # rewriting from relative to absolute path to make the curl work at the next recursion
+                else: # TODO for all non-local URLs, only collecting data 
+                    if verbose:
+                        print('\t' * indent_level + "|" + next_url)
                     result.append(next_url + " - EXTERNAL")
                     continue
         else: # skip anchors with no links
             continue
 
-def link_scrape(url, base_url, verbose=False):
+def link_scrape(url, verbose=False):
     result = dict()
     list = []
 
     # collecting statistics
     start_time = timeit.default_timer()
-    result['data'] = analyze_anchors(url, base_url, result=list, verbose=verbose)
+    result['data'] = analyze_anchors(url, url, result=list, verbose=verbose)
     end_time = timeit.default_timer()
 
     result['exec_time'] = end_time - start_time
@@ -73,5 +75,4 @@ def link_scrape(url, base_url, verbose=False):
   
 if __name__ == "__main__":
 #    test_curl("localhosts")
-    result = link_scrape("localhost", "localhost", verbose=True)
-    # print('\n'.join(result['data']))
+    result = link_scrape(sys.argv[1], verbose=True)
